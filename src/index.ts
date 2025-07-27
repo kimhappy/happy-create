728dfe99960ae1d7b52f00189affd6e506c84b10
kimhappy <kimhappyofficial@gmail.com>
2025-07-27 16:38:39 +0900

    ref: curry

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { StoreMutatorIdentifier, create } from 'zustand'
 
-type _AddKey< Creator, Key > = Creator extends (...args: infer P) => infer R ? (key: Key, ...args: P) => R : never
+type _AddKey< Creator, Key > = (key: Key) => Creator
 
 const _map = new Map()
 
@@ -10,7 +10,7 @@ export const createWithKey = <
   Mos extends [StoreMutatorIdentifier, unknown][] = []
 >(kreator: _AddKey< Parameters< typeof create< Store, Mos > >[ 0 ], Key >) => (key: Key) => {
   if (!_map.has(key)) {
-    _map.set(key, create< Store, Mos >((set, get, api) => kreator(key, set, get, api)))
+    _map.set(key, create< Store, Mos >((set, get, api) => kreator(key)(set, get, api)))
   }
 
   return _map.get(key)() as Store
